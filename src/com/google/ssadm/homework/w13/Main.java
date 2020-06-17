61391ae5aca8328067c6714fc86ce15d832a13a6
Oleksandr <ssadmin@gmail.com>
2020-06-17 19:11:27 +0300

    Homeworks 13

---
+++ src/com/google/ssadm/homework/w13/Main.java
@@ -0,0 +1,33 @@
+package com.google.ssadm.homework.w13;
+
+// Вычислить с помощью цикла факториал числа - n введенного с клавиатуры (4<n<16).
+// Факториал числа это произведение всех чисел от этого числа до 1.
+// Например 5!=5*4*3*2*1=120
+
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a positive number from 0 and above to find factorial");
+        int number = sc.nextInt();
+        switch (number) {
+            case 0 -> System.out.println("Factorial 0 is 1");
+            case 1 -> System.out.println("Factorial 1 is 1");
+            case 2 -> System.out.println("Factorial 2 is 2");
+            default -> {
+                System.out.printf("Factorial %d is %d", number, factorial(number));
+            }
+        }
+    }
+
+    public static BigInteger factorial(int i){
+        return IntStream
+                .rangeClosed(2, i) // stream of int from 2 to i
+                .mapToObj(BigInteger::valueOf) // stream of BigInteger objects
+                .reduce(BigInteger::multiply) // multiply all BigInteger
+                .get();
+    }
+}
