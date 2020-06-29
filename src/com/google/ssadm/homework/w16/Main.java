6767dd6888f67277712571a142153362a207c2ef
Oleksandr <ssadmin@gmail.com>
2020-06-29 20:05:26 +0300

    Add 16-17

---
+++ src/com/google/ssadm/homework/w16/Main.java
@@ -0,0 +1,34 @@
+package com.google.ssadm.homework.w16;
+
+import java.util.Scanner;
+
+// С помощью цикла (только одного) нарисовать такую фигуру.
+// Причем максимальная высота этой фигуры вводиться с клавиатуры
+// (в примере максимальная высота - 4)
+//  *
+//  **
+//  ***
+//  ****
+//  ***
+//  **
+//  *
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Please enter figure height:");
+        Scanner scanner = new Scanner(System.in);
+        int height = scanner.nextInt();
+
+        if(height >= 1){
+            for (int i = 1; i<= height*2-1; i++){
+                if (i<=height){
+                    System.out.println(new String(new char[i]).replace("\0", "*"));
+                } else {
+                    System.out.println(new String(new char[height*2-i]).replace("\0", "*"));
+                }
+            }
+        } else {
+            System.out.println("Invalid input");
+        }
+
+    }
+}
---
+++ src/com/google/ssadm/homework/w17/Main.java
@@ -0,0 +1,35 @@
+package com.google.ssadm.homework.w17;
+
+// С помощью циклов вывести на экран все простые числа от 1 до 100.
+// Простое число — число которое делиться нацело только на единицу или
+// само на себя. Первые простые числа это — 2,3,5,7...
+
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        int target = 100;
+        for(int i = 2; i <= target; i++){
+            if(isPrime(i)){
+                System.out.print(i + " ");
+            }
+            if(isPrime2(i)){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPrime(int check){
+        return !IntStream.rangeClosed(2, check / 2).anyMatch(i -> check % i == 0);
+    }
+
+    public static boolean isPrime2(int check){
+        if (check == 2 || check == 3) {
+            return true;
+        } else {
+            for (int i = 2; i <= Math.sqrt(check); i++)
+                if(check%i==0) return false;
+            return true;
+        }
+    }
+}
